4b40c1bb751c89089b3c1696913f73cfb4de8514
MBulti <bulthaupmoritz@goglemail.com>
2021-02-18 11:22:07 +0100

    added 3b

--- A2_Aufgabe_3b.c
+++ A2_Aufgabe_3b.c
@@ -13,24 +13,54 @@
 
 #include <stdio.h>
 
-#define USMILE 1.6093
-#define USGALLON 3.785
+#define USMILE 1.6093 // 1 mile = 1.6093 Km
+#define USGALLON 3.785 // 1 gallon = 3.785 Liter
 
 int main(void)
 {
     float lastOdoMeter = 0, currentOdoMeter = 0, tankFillValue = 0;
-    printf("Tachostand letzter Tankstop:\n");
+    int unitOfMeasurement = 0;
+    while (!(unitOfMeasurement == 1 || unitOfMeasurement == 2))
+    {
+        printf("Standort: 1. EU, 2. US\n");
+        scanf("%i", &unitOfMeasurement);
+    }
+
+    printf("Tachostand letzter Tankstop (km oder miles): ");
     scanf("%f", &lastOdoMeter);
 
-    printf("Tachostand aktueller Tankstop:\n");
+    printf("Tachostand aktueller Tankstop (km oder miles): ");
     scanf("%f", &currentOdoMeter);
 
-    printf("Wieviel Liter wurden getankt?\n");
+    printf("Menge getanktes Benzin (l oder gal): ");
     scanf("%f", &tankFillValue);
 
     float distance = currentOdoMeter - lastOdoMeter;
-    //float usage = tankFillValue / (distance / 100);
-    float usage = tankFillValue / distance * 100;
-    printf("\nAktueller Verbrauch ist %f L/100km", usage);
+    float secondDistance = 0, usage = 0, secondUsage = 0;
+
+    if(unitOfMeasurement == 1) // EU werte, dann "zweite" sind US
+    {
+        usage = tankFillValue / distance * 100;                             // Liter / KM * 100
+        secondDistance = distance / USMILE;                                 // KM / 1.6 = MILE
+        secondUsage = secondDistance / (tankFillValue / USGALLON);          // MILE / GALLON
+
+        printf("\nZurueckgelegte Distanz in KM: %f", distance);
+        printf("\nZurueckgelegte Distanz in Meilen: %f", secondDistance);
+
+        printf("\nAktueller Verbrauch in L/100 KM: %f", usage);
+        printf("\nAktueller Verbrauch in MPG: %f", secondUsage);
+    }
+    else if(unitOfMeasurement == 2) // US werte, dann "zweite" sind EU
+    {
+        usage = distance / tankFillValue;                                   // MILES / GALLON
+        secondDistance = distance * USMILE;                                 // MILE * 1.06 = KM
+        secondUsage = (tankFillValue * USGALLON) / secondDistance * 100;    // (Gallon * 3.7) / KM * 100)
+
+        printf("\nZurueckgelegte Distanz in KM: %f", secondDistance);
+        printf("\nZurueckgelegte Distanz in Meilen: %f", distance);
+
+        printf("\nAktueller Verbrauch in L/100 KM: %f", secondUsage);
+        printf("\nAktueller Verbrauch in MPG: %f", usage);
+    }
     return 0;
 }
